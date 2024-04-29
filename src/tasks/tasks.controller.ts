15ba41aa147456fe09d9bfecb363a3ee799fbdcb
Yevhen Spilnyk <>
2024-04-29 23:41:13 +0300

    save all CRUD

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from "@nestjs/common"
-import { TasksService } from "./tasks.service"
+import { Body, Controller, Get, Post, Param, Delete, Patch } from "@nestjs/common";
+import { TasksService } from "./tasks.service";
+import { Task, TaskStatus } from "./task.model";
+import { CreateTaskDto } from "./dto/create-task.dto";
 
 @Controller("tasks")
 export class TasksController {
     constructor(private tasksService: TasksService) {}
 
     @Get()
-    getAllTasks() {
-        return this.tasksService.getAllTasks()
+    getAllTasks(): Task[] {
+        return this.tasksService.getAllTasks();
+    }
+
+    @Get("/:id")
+    getTaskById(@Param("id") id: string): Task {
+        return this.tasksService.getTaskById(id);
+    }
+
+    @Post()
+    createTask(@Body() CreateTaskDto: CreateTaskDto): Task {
+        return this.tasksService.createTask(CreateTaskDto);
+    }
+
+    @Patch("/:id/status")
+    updateTaskStatus(@Param("id") id: string, @Body("status") status: TaskStatus): Task {
+        return this.tasksService.updateTaskStatus(id, status);
+    }
+
+    @Delete("/:id")
+    deleteTaskById(@Param("id") id: string): void {
+        this.tasksService.deleteTaskById(id);
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,43 @@
-import { Injectable } from "@nestjs/common"
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { Task, TaskStatus } from "./task.model";
+import { v4 as uuid } from "uuid";
+import { CreateTaskDto } from "./dto/create-task.dto";
 
 @Injectable()
 export class TasksService {
-    private tasks = []
+    private tasks: Task[] = [];
 
-    getAllTasks() {
-        return this.tasks
+    getAllTasks(): Task[] {
+        return this.tasks;
+    }
+
+    getTaskById(id: string): Task {
+        return this.tasks.find((task) => task.id === id);
+    }
+
+    createTask(createTaskDto: CreateTaskDto): Task {
+        const { title, description } = createTaskDto;
+        const task: Task = {
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.OPEN
+        };
+        this.tasks.push(task);
+        return task;
+    }
+
+    updateTaskStatus(id: string, taskStatus: TaskStatus): any {
+        const task = this.getTaskById(id);
+        if (taskStatus) {
+            task.status = taskStatus;
+        } else {
+            throw new HttpException("Please provide a status", HttpStatus.BAD_REQUEST);
+        }
+        return task;
+    }
+
+    deleteTaskById(id: string): void {
+        this.tasks = this.tasks.filter((task) => task.id !== id);
     }
 }
