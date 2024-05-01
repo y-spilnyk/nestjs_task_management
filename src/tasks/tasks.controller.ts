571a7d4299b5fbcf3cc10e17e6cbe0e6d8b4a452
Yevhen Spilnyk <>
2024-05-01 10:17:58 +0300

    modify the get "task" endpoint to be more flexible and find through the query params too

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,15 +1,20 @@
-import { Body, Controller, Get, Post, Param, Delete, Patch } from "@nestjs/common";
+import { Body, Controller, Get, Post, Param, Delete, Patch, Query } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
 import { Task, TaskStatus } from "./task.model";
 import { CreateTaskDto } from "./dto/create-task.dto";
+import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
 
 @Controller("tasks")
 export class TasksController {
     constructor(private tasksService: TasksService) {}
 
     @Get()
-    getAllTasks(): Task[] {
-        return this.tasksService.getAllTasks();
+    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+        if (Object.keys(filterDto).length) {
+            return this.tasksService.getTasksWithFilters(filterDto);
+        } else {
+            return this.tasksService.getAllTasks();
+        }
     }
 
     @Get("/:id")
@@ -18,8 +23,8 @@ export class TasksController {
     }
 
     @Post()
-    createTask(@Body() CreateTaskDto: CreateTaskDto): Task {
-        return this.tasksService.createTask(CreateTaskDto);
+    createTask(@Body() createTaskDto: CreateTaskDto): Task {
+        return this.tasksService.createTask(createTaskDto);
     }
 
     @Patch("/:id/status")
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { Task, TaskStatus } from "./task.model";
 import { v4 as uuid } from "uuid";
 import { CreateTaskDto } from "./dto/create-task.dto";
+import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
 
 @Injectable()
 export class TasksService {
@@ -9,6 +10,23 @@ export class TasksService {
 
     getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+        const { status, search } = filterDto;
+        let tasks = this.getAllTasks();
+        if (status) {
+            tasks = tasks.filter((data) => data.status === status);
+        }
+        if (search) {
+            tasks = tasks.filter((data) => {
+                if ([data.title, data.description].includes(search)) {
+                    return true;
+                }
+                return false;
+            });
+        }
+        return tasks;
     }
 
     getTaskById(id: string): Task {
