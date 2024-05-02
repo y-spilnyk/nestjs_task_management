815bb1c5caa1a5e43401dd43ab0b6abdf6440cb6
Yevhen Spilnyk <>
2024-05-02 21:45:22 +0300

    clean unnecessary imports

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
 import { TasksRepository } from "./tasks.repository ";
