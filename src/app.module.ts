d9b49b48e52074ab106c10df8f133fbbf4665b93
Yevhen Spilnyk <>
2024-05-07 18:36:47 +0300

    save type orm DB settings to the env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,23 @@ import { Module } from "@nestjs/common";
 import { TasksModule } from "./tasks/tasks.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Task } from "./tasks/task.entity";
-import { AuthModule } from './auth/auth.module';
-import { User } from "./auth/user.entity"
+import { AuthModule } from "./auth/auth.module";
+import { User } from "./auth/user.entity";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.env.stage.${process.env.STAGE}`
+        }),
         TasksModule,
         TypeOrmModule.forRoot({
             type: "postgres",
-            host: "localhost",
-            port: 5433,
-            username: "postgres",
-            password: "postgres",
-            database: "task-management",
+            host: process.env.DATABASE_HOST,
+            port: parseInt(process.env.DATABASE_PORT),
+            username: process.env.USERNAME,
+            password: process.env.PASSWORD,
+            database: process.env.DATABASE_NAME,
             autoLoadEntities: true,
             synchronize: true,
             entities: [Task, User]
